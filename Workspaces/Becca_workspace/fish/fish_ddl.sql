407b8581f7ccb1b82ccaccdd7359ceda7bde6e0b
Becca Blackham <rbi1830@gmail.com>
2023-05-22 12:40:27 -0600

    fish visuals on farmed/captured and protein

--- Workspaces/Becca_workspace/fish/fish_ddl.sql
+++ Workspaces/Becca_workspace/fish/fish_ddl.sql
@@ -1,4 +1,4 @@
---testing sql code
+--fish_data database
 
 use master;
 go
@@ -56,35 +56,3 @@ create table CountryAndFishFacts (
 );
 go
 
---insert into tables
-
---continents
-insert into Continents ([ContinentName])
-    select distinct continent
-    from TempFishAndCountry;
-go
-
-
---countries
-insert into Countries (CountryCode, CountryName, ContinentID)
-    select distinct temp.Code, temp.Entity, c.ContinentID
-    from Continents c 
-    inner join TempFishAndCountry temp 
-    on c.ContinentName = temp.continent;
-go
-
-
---CountryAndFishFacts
-
-insert into CountryAndFishFacts (Year, FishConsumedkg, FishProducedtons, Population, LifeExpectancy, CountryID)
-    select 
-        temp.Year,
-        temp.kg_per_yr_per_capita,
-        temp.Production_in_tons,
-        temp.Population_historical_estimates,
-        temp.Life_Expectancy_years,
-        c.CountryID
-        from TempFishAndCountry temp 
-        join Countries c 
-        on c.CountryName = temp.Entity;
-go
